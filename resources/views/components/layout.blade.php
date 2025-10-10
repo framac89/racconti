1b5c074aa9dca23a7883ea745afdc320c4b2b95c
Francesco Maceri <francescomaceri89@gmail.com>
2025-10-10 19:45:29 +0200

    bozza1home

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
+<body class="sfondoblog">
     <x-navbar/>
     {{ $slot }}
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,19 @@
 <x-layout>
-<h1>titolo pagina home</h1>
+<div class="container">
+    <div class="row my-4">
+        <div class="col-12">
+            <h1 class="text-center mt-2 titolo">Benvenuti nel blog di Roberta</h1>
+        </div>
+    </div>
+    <div class="row d-flex justify-content-evenly my-4">
+        <div class="col-5 d-flex flex-column justify-content-center align-items-center border border-dark rounded sfondoschede">
+            <h2>Articolo più recente</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolores ullam ea delectus unde necessitatibus praesentium quas? Eligendi a sapiente dignissimos laudantium molestiae rem nostrum facere, perferendis consequatur temporibus veritatis!</p>
+        </div>
+        <div class="col-2"></div>
+        <div class="col-5 d-flex flex-column justify-content-center align-items-center border border-dark rounded sfondoschede">
+            <h2>Articolo recente</h2> <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, ducimus cumque laudantium dolore tempora sed iure unde doloribus vel quasi sapiente corporis nisi. Nulla fuga a inventore consequuntur, molestiae in.</p>
+        </div>
+    </div>
+</div>
 </x-layout>
